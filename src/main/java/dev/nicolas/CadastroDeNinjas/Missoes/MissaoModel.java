b44430ce53a6b89295d558647a5743c542826d31
Nicolas Ferreira <nicolas.ferreira12.nf@gmail.com>
2026-02-12 09:09:32 -0300

    feat: add MissaoModel entity

---
+++ src/main/java/dev/nicolas/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -0,0 +1,26 @@
+package dev.nicolas.CadastroDeNinjas.Missoes;
+
+import dev.nicolas.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MissaoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String dificuldade;
+    @OneToMany(mappedBy = "missao")
+    private List<NinjaModel> ninjas;
+
+}
