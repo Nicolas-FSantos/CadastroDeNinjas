f84815d2580057330545a8d4e61c84cc50aa2e16
Nicolas Ferreira <nicolas.ferreira12.nf@gmail.com>
2026-02-12 09:14:54 -0300

    feat: relate NinjaModel with MissaoModel and apply lombok

--- src/main/java/dev/nicolas/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/nicolas/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,9 +1,16 @@
 package dev.nicolas.CadastroDeNinjas.Ninjas;
 
+import dev.nicolas.CadastroDeNinjas.Missoes.MissaoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -11,37 +18,9 @@ public class NinjaModel {
     private String nome;
     private String email;
     private int idade;
+    @ManyToOne
+    @JoinColumn(name = "missoes_id")
+    private MissaoModel missao;
 
-    public NinjaModel() {
-    }
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
